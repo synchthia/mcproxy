e414d60beb0fa2836f9611d7d111c1be4b18d441
Laica Lunasys <laica.lunasys@gmail.com>
2024-07-15 13:04:17 +0900

    Add velocity

---
+++ velocity/autoconfig.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+# Rewrite velocity.toml
+rewriteSettings() {
+    local TARGET="velocity.toml"
+    local key=$1
+    local value=$2
+
+    if [ "$value" == "" ]; then
+        [ "$DEBUG" != "" ] && echo "!! ${key} env has not provided. ignoring..."
+        return 0
+    fi
+
+    if [ $(echo -ne "$key" | grep "secret") ]; then
+        _display_value="<SECRET>"
+    else
+        _display_value="${value}"
+    fi
+
+    if grep "${key}" "$TARGET" > /dev/null; then
+        echo "- Overwrite settings: (${key}: ${_display_value})"
+        sed -i "/^${key}\s* = / c ${key}\ =\ ${value//\\/\\\\}" "$TARGET"
+    else
+        echo "- Append settings: (${key}: ${_display_value})"
+        echo "${key} = ${value}" >> "$TARGET"
+    fi
+}
+
+echo "=> Rewrite settings..."
+# Deprecated
+#rewriteSettings "forwarding-secret" "\"${VELOCITY_SECRET}\""
+echo -ne "${VELOCITY_SECRET}" > /app/server/forwarding.secret
+
+rewriteSettings "bind" "\"${BIND_ADDRESS:-0.0.0.0}:${PORT:-25565}\""
+rewriteSettings "show-max-players" "${MAX_PLAYERS:-5000}"
+rewriteSettings "online-mode" "${ONLINE_MODE:-true}"
---
+++ velocity/utils/velocity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+VERSION="$1"
+if [ "$VERSION" == "" ]; then
+    echo "Usage: <version>"
+    exit 1
+fi
+
+echo ":: Retrieve latest builds..."
+BUILD=$(curl -fsSL https://papermc.io/api/v2/projects/velocity/versions/$VERSION | jq '.builds | max')
+
+echo "-- Detected: $BUILD"
+
+echo ":: Downloading Velocity..."
+curl -o velocity.jar -fsSL https://papermc.io/api/v2/projects/velocity/versions/${VERSION}/builds/${BUILD}/downloads/velocity-${VERSION}-${BUILD}.jar
